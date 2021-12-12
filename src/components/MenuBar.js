e7adf9f5c188b95f398856d83aa347d371b22812
wilcorfis <wilmerandres505@gmail.com>
2021-12-12 12:15:43 -0500

    menu bars estudiante admin y lider

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -9,20 +9,20 @@ import { AuthContext } from '../context/auth';
 function MenuBar() {
   const { user, logout } = useContext(AuthContext);
   const [value, setValue] = useState("");
- 
-  let val=""
+
+  let val = ""
 
 
   useEffect(() => {
-  
-      setValue(val);
- 
+
+    setValue(val);
+
   }, [val]);
-    if (user){
+  if (user) {
 
     const obj = JSON.parse(JSON.stringify(user))
-    val=obj.perfil
-    
+    val = obj.perfil
+
   }
 
   const pathname = window.location.pathname;
@@ -33,7 +33,7 @@ function MenuBar() {
   const handleItemClick = (e, { name }) => setActiveItem(name);
   //activeItem === 'home'
 
-  const menuBar = user ?
+  const menuBar = user && value === "Lider" ?
 
     (
 
@@ -50,11 +50,12 @@ function MenuBar() {
           to="/"
         />
 
-
+        <Menu.Item name="Usuarios" active as={Link} to="/" />
         <Menu.Item name="Proyectos Liderados" active as={Link} to="/misproyectos" />
 
 
         <Menu.Item name="Mis inscripciones" active as={Link} to="/misisncripciones" />
+        <Menu.Item name="Avances" active as={Link} to="/" />
 
 
 
@@ -65,10 +66,43 @@ function MenuBar() {
 
 
 
-    ) : (
+    ) : user && value === "Administrador" ? (
       <Menu pointing secondary size="massive" color="teal">
+        <Menu.Item name={value} active as={Link} to="/" />
+
+        <Menu.Item name="Mi perfil" active as={Link} to="/miperfil" />
 
 
+        <Menu.Item name="administrar usuarios" active as={Link} to="/usuarios" />
+
+
+        <Menu.Item name="administrar proyectos" active as={Link} to="/proyectos" />
+        <Menu.Menu position="right">
+          <Menu.Item name="logout" onClick={logout} />
+        </Menu.Menu>
+
+
+
+   
+      </Menu>
+    ) : user && value === "Estudiante" ? (
+      <Menu>
+        <Menu.Item name={value} active as={Link} to="/" />
+
+        <Menu.Item name="Mi perfil" active as={Link} to="/miperfil" />
+
+
+        <Menu.Item name="proyetos" active as={Link} to="/" />
+
+
+        <Menu.Item name="avances" active as={Link} to="/p" />
+        <Menu.Menu position="right">
+          <Menu.Item name="logout" onClick={logout} />
+        </Menu.Menu>
+
+      </Menu>
+    ) :user===null &&(
+      <Menu pointing secondary size="massive" color="teal">
         <Menu.Menu position="right">
           <Menu.Item
             name="login"
@@ -86,6 +120,7 @@ function MenuBar() {
           />
         </Menu.Menu>
       </Menu>
+
     );
 
 
